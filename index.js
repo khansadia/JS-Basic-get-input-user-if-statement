219d9e871beaf3098480909607219f9ee023466a
Sadia Khan <khansadia220@gmail.com>
2024-01-11 10:15:23 +0100

    update the README.md with link to contributing guide ALSO updated js file

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ alert(" This is an alert message! Dismiss me before the program continue")
 
  // Prompt the user for the name. the answer that is return can be saved in a variable
  const nameOfUser=prompt ("Please enter your name");
- alert("Thankyou" + nameOfUser + "!");
  
+ // alert works some this 
+ 
+ alert("Thankyou" + nameOfUser + "!");
+ // Ask if user is older than 20 years old
+ const isOver20 = confirm("Are you over the age of 20?");
+ 
+ // Alert the answer.
+ alert(`Were you older then 20? ${isOver20}`);
+/* 
+ // Ask if user is older than 20 years old
+const isOver20 = window.confirm("Are you over the age of 20?");
+
+// Alert the answer.
+window.alert(`Were you older then 20? ${isOver20}`); */
